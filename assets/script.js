e248840efd21674baba3dc83b560f453b9f7ccf8
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-11 20:26:23 -0700

    started building search button function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,19 @@
 var testEl = $('#testContainer');
-var searchBtnEl = $('searchBtn');
+var searchBtnEl = $('#searchBtn');
+var searchFormEl = $('#search-form');
+var historyBtnsEl = $('#searchBtn');
 
-searchBtnEl.submit(myFunc);
+function handleSearch(event) {
+    event.preventDefault();
 
+    var city = $('input[id="searchBar"]').val();
+    console.log("value of city is = " + city);
+
+
+
+};
+
+searchFormEl.on('submit', handleSearch);
 
 // testEl.append(`
 
