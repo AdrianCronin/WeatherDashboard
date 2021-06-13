1ac7ffd2e8297d1e43f6083621033b680a60ad52
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-13 11:48:24 -0700

    finished renderCurrent function that takes the data.current array as an argument then renders data from that to the page

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var searchBtnEl = $('#searchBtn');
 var searchFormEl = $('#search-form');
 var historyBtnsEl = $('#historyBtns');
 var coords = {};
-
+var city = "";
 
 // this function will add more buttons
 function handleButtonAppend(event) {
@@ -24,10 +24,7 @@ searchFormEl.on('submit', handleButtonAppend);
 
 // take the user search input and makes a Current Weather API request to get the latitude and longitude of the city
 function getCoordApi() {
-    var city = "East Wenatchee"; //change this to user input
-    var container = $('#dailyWeather');
-
-    // change this to call user input
+    city = "East Wenatchee"; //change this to user input
     var coordApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
 
     fetch(coordApi)
@@ -35,7 +32,7 @@ function getCoordApi() {
             return response.json(); // make an error message if no response
         })
         .then(function (data) {
-            console.log(data);
+            // console.log(data);
 
             getForecastApi(data.coord); // have to call this function here due to fetch being asynchronous
 
@@ -79,7 +76,7 @@ function renderForecast(arr) {
             <img src="${icon}">
             <p class="subtitle is-6">Temp: ${temp} °F</p>
             <p class="subtitle is-6">Wind: ${wind} MPH</p>
-            <p class="subtitle is-6">Wind: ${humid} %</p>
+            <p class="subtitle is-6">Humidity: ${humid} %</p>
         `);
     };
 };
@@ -88,22 +85,30 @@ function renderForecast(arr) {
 
 // take the data.current[] array as an argument then uses it to create the forecast boxes
 function renderCurrent(arr) {
-    for (i = 1; i < arr.length; i++) {
-        var date = new Date(arr[i].dt * 1000).toLocaleDateString("en-US"); // convert into miliseconds
-        var container = $('currentWeather');
-        var icon = "https://openweathermap.org/img/w/" + arr[i].weather[0].icon + ".png";
-        var temp = arr[i].temp.day;
-        var wind = arr[i].wind_speed;
-        var humid = arr[i].humidity;
-        // render each day to page
-        container.append(`
-            <p class="title is-4">${date}</p>
-            <img src="${icon}">
-            <p class="subtitle is-6">Temp: ${temp} °F</p>
-            <p class="subtitle is-6">Wind: ${wind} MPH</p>
-            <p class="subtitle is-6">Wind: ${humid} %</p>
-        `);
+    var date = new Date(arr.dt * 1000).toLocaleDateString("en-US"); // convert into miliseconds
+    var container = $('#currentWeather');
+    var icon = "https://openweathermap.org/img/w/" + arr.weather[0].icon + ".png";
+    var temp = arr.temp;
+    var wind = arr.wind_speed;
+    var humid = arr.humidity;
+    var uvIndex = arr.uvi;
+    var color;
+    if (uvIndex > 5) {
+        color = "uvRed"
+    } else if (uvIndex < 5 && uvIndex > 2) {
+        color = "uvYellow"
+    } else {
+        color = "uvGreen"
     };
+
+    // make UV index change color
+    container.append(`
+        <p class="title is-3">${city} (${date})<img src="${icon}"></p>
+        <p class="subtitle is-6">Temp: ${temp} °F</p>
+        <p class="subtitle is-6">Wind: ${wind} MPH</p>
+        <p class="subtitle is-6">Humidity: ${humid} %</p>
+        <p class="subtitle is-6">UV Index: <span class="${color}"> ${uvIndex}</span></p>
+        `);  
 };
 
 
