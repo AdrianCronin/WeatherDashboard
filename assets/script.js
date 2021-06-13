ad6ba816828b97a940211f7260a1d2edd80d6914
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-13 11:02:43 -0700

    renderForecast function now functions so that it displays each day's weather to the page

--- assets/script.js
+++ assets/script.js
@@ -24,9 +24,8 @@ searchFormEl.on('submit', handleButtonAppend);
 
 // take the user search input and makes a Current Weather API request to get the latitude and longitude of the city
 function getCoordApi() {
-    var city = "East Wenatchee";
+    var city = "East Wenatchee"; //change this to user input
     var container = $('#dailyWeather');
-
 
     // change this to call user input
     var coordApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
@@ -37,14 +36,6 @@ function getCoordApi() {
         })
         .then(function (data) {
             console.log(data);
-
-            // url for the current weather icon
-            var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-
-            // make a renderDaily function that takes the data obj
-            container.append(`
-            <p class="title is-3">${city}<img src="${icon}"></p>
-            `);
 
             getForecastApi(data.coord); // have to call this function here due to fetch being asynchronous
 
@@ -57,7 +48,7 @@ function getCoordApi() {
 
 // takes the data.coord object as an argument and makes a One Call API request
 function getForecastApi(obj) {
-    var forecastApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + obj.lat + "&lon=" + obj.lon + "&exclude=current,minutely,hourly&appid=" + APIkey;
+    var forecastApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + obj.lat + "&lon=" + obj.lon + "&exclude=minutely,hourly&units=imperial&appid=" + APIkey;
 
     fetch(forecastApi)
         .then(function (response) {
@@ -65,7 +56,9 @@ function getForecastApi(obj) {
         })
         .then(function (data) {
 
-            console.log(data.daily);
+            console.log(data.current);
+
+            renderCurrent(data.current);
             renderForecast(data.daily);
         });
 };
@@ -73,12 +66,45 @@ function getForecastApi(obj) {
 
 // take the data.daily[] array as an argument then uses it to create the forecast boxes
 function renderForecast(arr) {
+    for (i = 1; i < arr.length; i++) {
+        var date = new Date(arr[i].dt * 1000).toLocaleDateString("en-US"); // convert into miliseconds
+        var container = $('#day' + i);
+        var icon = "https://openweathermap.org/img/w/" + arr[i].weather[0].icon + ".png";
+        var temp = arr[i].temp.day;
+        var wind = arr[i].wind_speed;
+        var humid = arr[i].humidity;
+        // render each day to page
+        container.append(`
+            <p class="title is-4">${date}</p>
+            <img src="${icon}">
+            <p class="subtitle is-6">Temp: ${temp} °F</p>
+            <p class="subtitle is-6">Wind: ${wind} MPH</p>
+            <p class="subtitle is-6">Wind: ${humid} %</p>
+        `);
+    };
+};
 
-    var date = new Date(arr[1].dt * 1000).toLocaleDateString("en-US");
 
 
-
-}
+// take the data.current[] array as an argument then uses it to create the forecast boxes
+function renderCurrent(arr) {
+    for (i = 1; i < arr.length; i++) {
+        var date = new Date(arr[i].dt * 1000).toLocaleDateString("en-US"); // convert into miliseconds
+        var container = $('currentWeather');
+        var icon = "https://openweathermap.org/img/w/" + arr[i].weather[0].icon + ".png";
+        var temp = arr[i].temp.day;
+        var wind = arr[i].wind_speed;
+        var humid = arr[i].humidity;
+        // render each day to page
+        container.append(`
+            <p class="title is-4">${date}</p>
+            <img src="${icon}">
+            <p class="subtitle is-6">Temp: ${temp} °F</p>
+            <p class="subtitle is-6">Wind: ${wind} MPH</p>
+            <p class="subtitle is-6">Wind: ${humid} %</p>
+        `);
+    };
+};
 
 
 
